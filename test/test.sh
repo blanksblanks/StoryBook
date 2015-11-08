1faf3d93b055b6f54fcfe6f296ed003a5790ac4c
aalawson <a.a.lawson95@gmail.com>
2015-11-07 20:50:00 -0500

    fixing test bug

--- test/test.sh
+++ test/test.sh
@@ -5,20 +5,18 @@ make clean
 cd test/
 echo "Accept Tests:" >> test_results.txt
 for filename in tree*accept.txt; do
-        echo "$filename" >> test_results.txt
         menhir --interpret ../parser.mly < "$filename" | { 
-        if grep -q "REJECT"; then echo "FAILURE: $filename" >> test_results.txt;
-        else echo "SUCCESS: $filename" >> test_results.txt;
+        if grep -q "ACCEPT"; then echo "SUCCESS: $filename" >> test_results.txt;
+        else echo "FAILURE: $filename" >> test_results.txt;
         fi
         }
 done
 echo "****************************" >> test_results.txt
 echo "Reject Tests:" >> test_results.txt
 for filename in tree*reject.txt; do
-        echo "$filename" >> test_results.txt
         menhir --interpret ../parser.mly < "$filename" | { 
-        if grep -q "ACCEPT"; then echo "FAILURE: $filename" >> test_results.txt;
-        else echo "SUCCESS: $filename" >> test_results.txt;
+        if grep -q "REJECT"; then echo "SUCCESS: $filename" >> test_results.txt;
+        else echo "FAILURE: $filename" >> test_results.txt;
         fi 
         }
 done
