4a6593271943c39b5ce9d17dda6aefba8e538f64
aalawson <a.a.lawson95@gmail.com>
2015-11-14 18:41:43 -0500

    small error in code gen, can't get it to compile

--- codegen.ml
+++ codegen.ml
@@ -3,20 +3,48 @@ open Sast
 open Semantic_Analyzer
 open Lexing
 
-let sast =
+    let sast =
 	let lexbuf = Lexing.from_channel stdin in
 	let ast = Parser.program Scanner.token lexbuf in 
 	analyze_semantics ast
 
+
+
+let type_as_string t = match t with
+  |Sast.Number -> "int"
+  |Sast.Boolean -> "bool"
+  |_ -> "int"
+
+let write_params params f = 
+	fprintf f " ( ";
+    List.iter (fun v -> fprintf " %s " v.vname) f.fformals
+	fprintf f ") ";
+
+(* Takes expression tuple and file to write to *)
+let write_expr (e, t) f = match e with
+| Sast.LitString(s) -> fprintf f "%s" s
+| _ -> fprintf f "";
+
+let write_stmt stmt s f = match stmt with
+| Sast.Expression(e) -> write_expr e f
+| _ -> write_expr (LitString(""), Sast.String) f;
+
+let write_func funcdec f =
+	fprintf f "%s " type_as_string f.fname;
+	if f.fname = "plot" then fprintf f " main" else fprintf f " %s" f.name;
+	write_params f.formals f;
+	fprintf f " { \n";
+	List.iter (fun s -> write_stmt s f) f.body;
+	fprintf f " } \n";
+
 let generate_code pgm =
-let _ = print_string "#include <cstdio> Program {\n\n\t" in
+	let (cdecs, funcdecs) = pgm in 
+    let cfile = open_out "HelloWorld.c" in
+    fprintf cfile "#include <cstdio> Program {\n\n\t";
+    fprintf cfile "int main ()";
+    List.iter (fun f -> write_func f cfile) funcsdecs;
+    close_out cfile;
 
-
-let (classes, funcs) = pgm in
-		(*List.iter print_class_decl (List.rev classes); *)
-		List.iter print_func_decl (List.rev funcs);
-		print_string "\n}\n"
-
-in generate_code sast
+let _ = generate_code sast;
 
 
