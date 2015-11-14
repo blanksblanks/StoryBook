49201b9d395a26709fda4f01e18152428ca3ff4d
Nina Baculinao <nb2406@columbia.edu>
2015-11-13 19:35:13 -0500

    Check program and function processing

--- semantic_analyzer.ml
+++ semantic_analyzer.ml
@@ -8,11 +8,11 @@ type translation_environment = {
 
 type symbol_table = {
 	parent : symbol_table option;
-	variables : (string * var_decl * var_types) list;
+	variables : (string * var_decl * var_type) list;
 	functions : function_decl list;
-	objects : class_decl list;
+	classes : class_decl list;
   actions : action_decl list;
-  found_plot: bool;
+  return: bool;
 }
 
 let print_func = {
@@ -23,8 +23,11 @@ let print_func = {
   fbody = [];
 }
 
-let find_func( l : function_decl list) f =
-  List.find(fun c -> c.fname = f)
+let find_func(l : function_decl list) f =
+  List.find(fun k -> k.fname = f)
+
+let find_class(c : class_decl list) class =
+	List.find(fun k -> k.cname = class) c
 
 let rec check_id (scope : symbol_table) id =
 	try
@@ -69,3 +72,111 @@ let rec check_expr (scope : symbol_table) (expr : Ast.expr) = match expr with
 					| _ -> raise (Failure "Print only takes one argument")
         )
       | _ -> raise(Failure "Not a call")
+
+let rec check_var_type (scope : symbol_table) (v : Ast.var_type) = match v with
+	Ast.Number -> Sast.Number
+	| Ast.String -> Sast.String
+	| Ast.Boolean -> Sast.Boolean
+	| Ast.Char -> Sast.Char
+	| Ast.Object(id) ->
+		(try
+			let c = find_class scope.classes id in
+			Sast.Object(c)
+		with Not_found -> raise (Failure ("Character " ^ id ^ " not found.")))
+
+let rec check_func_stmt (scope : symbol_table) (stmt_lst : Sast.stmt list) (ftype : Sast.var_types) =
+	List.iter (
+		fun s -> match s with
+		Sast.Block (stmt_list) ->
+			check_func_stmt scope stmt_list ftype
+		| Sast.Return(e) ->
+			let (_, t) = e in
+			if t <> ftype then raise (Failure "Return statement returns the wrong type.") else ()
+		| Sast.If(_, s1, s2) ->
+			check_func_stmt scope [s1] ftype; check_func_stmt scope [s2] ftype
+		| Sast.For(_, _, _, s) ->
+			check_func_stmt scope [s] ftype
+		| Sast.While(_, s) ->
+			check_func_stmt scope [s] ftype
+		| _ -> ()
+	) stmt_lst
+
+let check_func_decl (env : translation_environment) (f : Ast.func_decl) =
+	let scope' = { env.scope with parent = Some(env.scope); variables = []; return_found = false } in
+	let rettype = check_var_type env.scope f.freturn in
+	let formals = List.fold_left (
+		fun k f -> match f with
+		Ast.Parameter(rettype, strng) -> (* Parameter of var_type * string *)
+			let rettype = check_var_type scope' rettype in
+			scope'.variables <- (strng, Sast.Variable(rettype, strng), rettype) :: scope'.variables; (Sast.Variable(rettype, strng), rettype) :: k
+	) [] f.fformals in
+	let statements = process_func_stmt scope' f.body rettype in
+	(* if scope'.return_found then; we may not need return_found if we do not have voids *)
+	let f = { fname = f.fname; fformals = formals; freturn = rettype; fbody = statements } in
+	env.scope.functions <- f :: env.scope.functions; (* throw away scope of function *) f
+	else
+		raise (Failure ("No return for Chapter " ^ f.fname ^ " when return expected.")))
+
+let process_func_stmt (scope : symbol_table) (stmt_lst : Ast.stmt list) (ftype : Sast.var_types) =
+	List.fold_left (
+		fun a s -> let stmt = check_stmt scope s in
+		match stmt with
+		Sast.Block (stmt_list) ->
+			check_func_stmt scope stmt_list ftype; stmt :: a
+		| Sast.Return(e) ->
+			let (_, t) = e in
+			if t <> ftype then raise (Failure "Return statement returns the wrong type.") else
+			scope.return_found <- true; stmt :: a
+		| Sast.If(_, s1, s2) ->
+			check_func_stmt scope [s1] ftype; check_func_stmt scope [s2] ftype; stmt :: a
+		| Sast.For(_, _, _, s) ->
+			check_func_stmt scope [s] ftype; stmt :: a
+		| Sast.While(_, s) ->
+			check_func_stmt scope [s] ftype; stmt :: a
+		| _ -> stmt :: a
+	) [] stmt_lst
+
+let process_func_decl (env : translation_environment) (f : Ast.func_decl) =
+	try
+		let _ = find_func env.scope.functions f.fname in
+			raise (Failure ("Function already declared with name " ^ f.fname))
+	with Not_found ->
+		if f.fname = "say" then raise (Failure "A function cannot be named" ^ f.fname)
+		else
+			if f.fname = "Plot" then
+				(
+					if f.freturn <> Number || (List.length f.fformals) <> 0 then
+					raise (Failure "Plot function must return numbers and cannot contain parameters")
+					else
+						let func = check_func_decl env f in
+						env.found_plot <- true; func
+					)
+			else
+				check_func_decl env f
+
+(* let process_class_decl (env : translation_environment) (c : Ast.class_decl) =
+	try
+		let _ = find_class env.scope.classes c.cname in
+			raise (Failure ("Class already declared with name " ^ c.cname))
+	with Not_found ->
+		let scope' = { env.scope with parent = Some(env.scope); variables = [] } in
+		let formals =
+		let instvars = List.fold_left ( fun a v -> process_var_decl scope' v :: a ) [] c.variable_decls in
+		let actions =
+		(* cname, cformals, cinstvars, cactions; param_decl, var decl, action_decl *)
+		env.scope.classes <- c :: env.scope.classes; c *)
+
+let check_program (p : Ast.program) =
+	let s = { parent = None; classes = []; functions = []; return_plot = false } in
+	let env = { scope = s; found_plot = false } in
+	let (classes, funcs) = p in
+	(* let classes =
+		List.fold_left (
+			fun a s -> process_class_decl env s :: a
+		) [] classes in *)
+	let classes = [] in
+	let funcs =
+		List.fold_left (
+			fun a f -> process_func_decl env f :: a
+		) [] (List.rev funcs) in
+  (if env.found_plot then classes, funcs else (raise (Failure "No Plot defined.")))
