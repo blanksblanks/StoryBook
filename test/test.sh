bcef21624ec54efba054aadeb2770a30b3ac8c09
aalawson <a.a.lawson95@gmail.com>
2015-11-28 14:18:41 -0600

    Moved tests to directory, fixed broken tests

--- test/test.sh
+++ test/test.sh
@@ -2,10 +2,10 @@
 
 cd ../
 make clean
-cd test/
+cd test/tree
 echo "Accept Tests:" >> test_results.txt
 for filename in tree*accept.txt; do
-        menhir --interpret ../parser.mly < "$filename" | { 
+        menhir --interpret ../../parser.mly < "$filename" | { 
         if grep -q "ACCEPT"; then echo "SUCCESS: $filename" >> test_results.txt;
         else echo "FAILURE: $filename" >> test_results.txt;
         fi
@@ -14,7 +14,7 @@ done
 echo "****************************" >> test_results.txt
 echo "Reject Tests:" >> test_results.txt
 for filename in tree*reject.txt; do
-        menhir --interpret ../parser.mly < "$filename" | { 
+        menhir --interpret ../../parser.mly < "$filename" | { 
         if grep -q "REJECT"; then echo "SUCCESS: $filename" >> test_results.txt;
         else echo "FAILURE: $filename" >> test_results.txt;
         fi 
