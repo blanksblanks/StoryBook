cc8a96ec98cf4f5a05490a6a113fd952d23bee24
aalawson <a.a.lawson95@gmail.com>
2015-11-28 21:32:22 -0600

    Test suite checks whether accepts were accepted and reject tests were rejected. Files must be named accordingly (see examples). Code gen now just writes to stdout and storybook/test suite redirects it to the appropriate file (not just helloWorld.c anymore).

--- test/test.sh
+++ test/test.sh
@@ -5,27 +5,55 @@ make clean
 make
 
 cd test
-for sbkname in *.sbk;do
-        program=`basename $sbkname .sbk`
-        .././run < "$sbkname" > "${program}.c"
+echo "Accept Tests:" >> test_results.txt
+for acceptname in *_Accept.sbk;do
+        program=`basename $acceptname _Accept.sbk`
+        echo "Test: $program" >> errors.txt
+        .././run < "$acceptname" > "${program}.c" 2>> errors.txt
         if [ -s "$program.c" ]
         then
           gcc -g $program.c -o $program
           if [ -f "$program" ]
           then
-            ./$program
             ./$program > "${program}Out.txt"
-            diff "${program}Out.txt" "${program}Exp.txt"
-          else 
+            diff "${program}Out.txt" "${program}_Exp.txt"
+            rm $program
+            echo "SUCCESS: $program" >> test_results.txt;
+          else
             echo "      /\_/\\ !! _
             =( °∩° )= //
               )   (  //
-             (__ __)//"          
+             (__ __)//"  
+             echo "FAILURE: $program" >> test_results.txt; 
+             echo "FAILURE: $program"       
           fi
         else
         echo "          /\_/\\ !! _
         =( °∩° )= //
           )   (  //
          (__ __)//"
+        echo "FAILURE: $program" >> test_results.txt; 
+        echo "FAILURE: $program"   
         fi
 done
+
+for rejectname in *_Reject.sbk;do
+        program=`basename $rejectname _Reject.sbk`
+        echo "Test: $program" >> errors.txt
+        .././run < "$rejectname" > "${program}.c" 2>> errors.txt
+        if [ ! -s "$program.c" ]
+        then
+          echo "SUCCESS: $program" >> test_results.txt
+          rm "$program.c"
+          
+        else
+          echo "FAILURE: $program" >> test_results.txt
+          echo "FAILURE: $program"
+        echo "          /\_/\\ !! _
+        =( °∩° )= //
+          )   (  //
+         (__ __)//"
+        fi
+done
+
+rm -rf *.dSYM
